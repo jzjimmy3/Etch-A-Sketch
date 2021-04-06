08401a9526dd8882007a0dbdae6ce13a2da26df1
Jimmy Zhang <Jzjimmy3@gmail.com>
2021-04-06 12:48:47 -0400

    Worked on hover functionality

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ function makeGrid(rows, cols) {
     container.style.setProperty("--grid-cols", cols)
     for (i = 0; i < (rows * cols); i++) {
         let grid = document.createElement('div')
-        grid.innerText = (i + 1);
+        // grid.innerText = (i + 1);
         container.appendChild(grid).className = "grid-item";
     };
 };
 
 makeGrid(16, 16)
+
+
+// function fillGrid(rows, cols) {
+//     const grids = document.querySelector(".grid-item")
+//     for (let i = 0; i < (rows * cols); i++) {
+//         container.addEventListener("mouseover", function (color) {
+//             grids.style.setProperty("background-color", "red")
+//         }   
+//     }
+// }
+
+// fillGrid(16, 16)
 
