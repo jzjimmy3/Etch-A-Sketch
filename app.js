5defbe2e6d29cc9b254f10e3dd1940e4ee6b3a65
Jimmy Zhang <Jzjimmy3@gmail.com>
2021-04-08 10:07:12 -0400

    Changed grid layout with full functionality

--- app.js
+++ app.js
@@ -3,30 +3,44 @@ const container = document.getElementById("container");
 function makeGrid(rows, cols) {
     container.style.setProperty("--grid-rows", rows)
     container.style.setProperty("--grid-cols", cols)
+
     for (i = 0; i < (rows * cols); i++) {
-        let grid = document.createElement('div')
-        // grid.innerText = (i + 1);
+        const grid = document.createElement('div')
         container.appendChild(grid).className = "grid-item";
     };
 };
 
-makeGrid(16, 16)
+makeGrid(3, 3)
+hover()
+function clearGrid() {
+    const gridArray = Array.from(container.childNodes);
+    gridArray.forEach((element) => {
+        container.removeChild(element);
+    });
+}
 
-const grids = document.querySelectorAll(".grid-item")
-grids.forEach(item => {
-    item.addEventListener('mouseover', event => {
-        item.style.setProperty("background-color", "red")
+function hover() {
+    const grids = document.querySelectorAll(".grid-item")
+    grids.forEach(item => {
+        item.addEventListener('mouseover', event => {
+            item.style.setProperty("background-color", "red")
+        })
     })
-})
+}
 
-// function reset() {
-//     alert('Hello')
-// }
 const reset = document.getElementById("reset");
 reset.addEventListener('click', event => {
+    const grids = document.querySelectorAll(".grid-item")
     grids.forEach(item => {
         item.style.setProperty("background-color", "white")
     })
+})
+
+const small = document.getElementById("small");
+small.addEventListener('click', event => {
+    clearGrid()
+    makeGrid(10, 10)
+    hover()
 })
 
 
